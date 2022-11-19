9bf3f43b56e7dc9ea5d6dc6b6ce5340dcf24a6e5
jb9229 <jb9229@gmail.com>
2022-11-19 12:05:53 +0900

    feat: edit home text

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Welcome to react-native-Seoul Our group will present under the theme of "Automating App Build and Deploy with Fastlane</Text>
       <StatusBar style="auto" />
     </View>
   );
